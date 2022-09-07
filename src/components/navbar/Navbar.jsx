fd81781b76ca6008496321c28dac9f7be352d9b6
Manish Kumar Gupta <manishkumarpatna2020@gmail.com>
2022-09-07 11:40:22 +0530

    React Hotle Booking App-ui

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import "./navbar.css";
+
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="navContainer">
+      <a href="/" className="softman-logo">Softman's Hotel</a>
+       
+
+        <div className="navItems">
+          <button className="navButton">Register</button>
+          <button className="navButton">Login</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
